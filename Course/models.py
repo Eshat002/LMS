9def98e72ab1d98b228ea70be533d75d808e439a
eshat002 <eshatjubayer22@outlook.com>
2024-07-27 02:25:37 +0600

    project setup and added Course model

---
+++ Course/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+Course_types = [('ux design', 'UX DESIGN'), ('video editing', 'VIDEO EDITING'),
+                ('web dev', 'WEB DEV')]
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    creator = models.CharField(max_length=200)
+    type = models.CharField(choices=Course_types,
+                            max_length=20,
+                            default='web dev')
+    start_date = models.DateTimeField(auto_now_add=False)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    bookmark = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name[:40]
---
+++ Course/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+x = 99
