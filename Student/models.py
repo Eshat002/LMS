528dcd541b37cda69cf8266b63ea438dcca944a1
eshat002 <eshatjubayer22@outlook.com>
2024-08-02 01:28:06 +0600

    fix model

--- Student/models.py
+++ Student/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
 
+student_types = [
+    ("avarage", "Avarage"),
+    ("good", "Good"),
+    ("best", "Best"),
+]
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     progress = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
+    score = models.DecimalField(max_digits=3, decimal_places=2, default=10.0)
+    better_than = models.PositiveIntegerField(
+        validators=[MaxValueValidator(100)], default=0
+    )
+    type = models.CharField(choices=student_types, max_length=20, default="good")
 
     class Meta:
         ordering = ["-id"]
